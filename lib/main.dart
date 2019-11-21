695de8c14fd0fe61fbf7f610c683516294f9753f
wanderwmarques <wanderwmarques@outlook.com>
2019-11-21 00:43:43 -0300

    form

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_aog/widgets/input.widget.dart';
 import 'package:flutter_aog/widgets/logo.widget.dart';
+import 'package:flutter_aog/widgets/success.widget.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+import 'widgets/loadin-button.widget.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+// This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,6 +23,10 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+// Importo controller da mascara
+  var _gasCtrl = new MoneyMaskedTextController();
+  var _alcCtrl = new MoneyMaskedTextController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +34,14 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
+          Success(reset: () {}, result: "Compensa utilizar x"),
+          Input(ctrl: _gasCtrl, label: "Gasolinha"),
+          Input(ctrl: _alcCtrl, label: "Alcool"),
+          LoadingButton(
+            text: "Calcular",
+            invert: false,
+            busy: true,
+          ),
         ],
       ),
     );
